92103eacd977619ed24d6060e518f16b2c9a8d3b
Alexander <alh97495@gmail.com>
2025-09-29 01:35:48 +0200

    fix e2e

--- cypress/e2e/homepage.spec.ts
+++ cypress/e2e/homepage.spec.ts
@@ -2,22 +2,17 @@
 
 describe("HomePage E2E", () => {
   it("отображает список персонажей и работает поиск", () => {
-    // Замени на URL локального dev-сервера
-    cy.visit("http://localhost:5173");
+    cy.visit("https://cozy-youtiao-11183e.netlify.app/");
 
-    // Проверяем заголовок
     cy.contains("Star Wars Characters").should("exist");
 
-    // Проверяем, что есть хотя бы один CharacterCard
     cy.get("[data-testid^=character-card]").should(
       "have.length.greaterThan",
       0
     );
 
-    // Вводим текст в поиск
     cy.get("input[type=text]").type("Luke");
 
-    // Проверяем, что фильтр применяется (например, только Luke Skywalker)
     cy.contains("Luke Skywalker").should("exist");
   });
 });
